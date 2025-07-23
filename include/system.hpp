030cdec8cf25408f95fa15b3b2b96770befff851
Siuol <nguyencaotan03072000@gmail.com>
2025-07-23 12:35:38 +0300

    Update system

---
+++ include/system.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   system.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/23 12:30:42 by siuol             #+#    #+#             */
+/*   Updated: 2025/07/23 12:35:16 by siuol            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <vector>
+
+// Color codes
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define CYAN    "\033[36m"
+
+// Logging macros
+#define LOG_SUCCESS(msg) std::cout << GREEN << msg << RESET
+#define LOG_ERROR(msg)   std::cerr << RED << msg << RESET << std::endl
+#define LOG_WARNING(msg) std::cout << YELLOW << msg << RESET << std::endl
+#define LOG_INFO(msg)    std::cout << BLUE << msg << RESET << std::endl
+#define LOG_DEBUG(msg)   std::cout << CYAN << msg << RESET << std::endl
