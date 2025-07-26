07aeca109d2b299dd06ef6a6cba783abbbe51554
Siuol <nguyencaotan03072000@gmail.com>
2025-07-26 03:38:57 +0300

    cmdJOIN server handler

--- src/server/implementation/sv_validation.cpp
+++ src/server/implementation/sv_validation.cpp
@@ -6,7 +6,7 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 10:48:25 by siuol             #+#    #+#             */
-/*   Updated: 2025/07/25 11:40:16 by siuol            ###   ########.fr       */
+/*   Updated: 2025/07/26 03:38:52 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ bool    Server::isServerChannel(const Channel* channel)
     return (this->_channelList.count(channel->getChannelName()));
 }
 
-bool    Server::isServerChannelName(std::string& channelName)
+bool    Server::hasServerChannel(std::string& channelName)
 {
     return (this->_channelList.count(channelName));
 }
