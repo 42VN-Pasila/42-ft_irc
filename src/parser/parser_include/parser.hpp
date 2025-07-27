9b750f649ad0ea551f5b079d12a6ad89cb2c7731
Triet Pham <pmtriet1211@gmail.com>
2025-07-27 12:53:05 +0300

    add executor demand text

---
+++ src/parser/parser_include/parser.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tripham <tripham@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/27 12:36:18 by tripham           #+#    #+#             */
+/*   Updated: 2025/07/27 12:40:08 by tripham          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "system.hpp"
+
+#define enum JOIN, PART, PRIVMSG
+
+void	parser::join
+
+void parser::privmsg
+
+[] pointer to function
