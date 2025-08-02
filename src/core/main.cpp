f719b9cef6be86f1e28bf1f7a6fc578858fc7c0c
Siuol7 <nguyencaotan03072000@gmail.com>
2025-08-02 18:45:25 +0300

    Update temp main

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 18:24:18 by caonguye          #+#    #+#             */
-/*   Updated: 2025/08/02 18:25:58 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/08/02 18:27:17 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@ int main(int ac, char** av)
     try
     {
         prs_program(ac, av);
+        // Tao Server()
+        // ket noi socket vao server
     }
     catch(const std::exception& e)
     {
         LOG_RED(e.what());
         std::cout << std::endl;
-    }
-    
+    } 
 }
