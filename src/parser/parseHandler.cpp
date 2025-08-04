2b8d65ea29951ffd0590029ebf7009a566e6b908
Siuol <nguyencaotan03072000@gmail.com>
2025-08-05 00:54:19 +0300

    Update parseJoin

--- src/parser/parseHandler.cpp
+++ src/parser/parseHandler.cpp
@@ -6,7 +6,7 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 10:49:51 by siuol             #+#    #+#             */
-/*   Updated: 2025/08/04 03:20:17 by siuol            ###   ########.fr       */
+/*   Updated: 2025/08/05 00:54:12 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,13 @@ void    Server::parseJoin(Client* client, std::string fullCommand)
     
     cmdPack = parseSplit(fullCommand);
     channelPack = parseSplitComma(cmdPack[1]);
-    passwordPack = parseSplitComma(cmdPack[2]);
+    
+    if (cmdPack.size() > 2)
+        passwordPack = parseSplitComma(cmdPack[2]);
+    for (int i = 0; i  < channelPack.size(); i++)
+    {
+        std::string password = (i < passwordPack.size() ? passwordPack[i] : "");
+        this->handlerJoin(client, channelPack[i], password);
+    }
 }
+
