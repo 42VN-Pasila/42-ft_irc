3ca292efaf2c69512bc10be332a774f12f4c5bd9
Siuol <nguyencaotan03072000@gmail.com>
2025-08-05 00:59:19 +0300

    Update parse system

--- include/server.hpp
+++ include/server.hpp
@@ -6,7 +6,7 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 10:32:41 by siuol             #+#    #+#             */
-/*   Updated: 2025/08/04 03:03:19 by siuol            ###   ########.fr       */
+/*   Updated: 2025/08/05 00:59:07 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,8 @@ class Server
 
         //ParseCmd
         void    parseJoin(Client* client, std::string fullCommand);
+        void    parseMultiTargets(Client* client, std::string fullCommand);
+        void    parseSingleTargets(Client* client, std::string fullCommand);
         
         //Exec
         void    execCommand(Client* client, std::string cmd, std::string fullCommand);
