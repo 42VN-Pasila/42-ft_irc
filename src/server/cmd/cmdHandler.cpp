5d0c7652ebb14d021ba905847f6f685aac1264a2
Siuol <nguyencaotan03072000@gmail.com>
2025-09-07 22:24:36 +0300

    Update noti error

--- src/server/cmd/cmdHandler.cpp
+++ src/server/cmd/cmdHandler.cpp
@@ -6,7 +6,7 @@
 /*   By: siuol <siuol@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 10:57:54 by siuol             #+#    #+#             */
-/*   Updated: 2025/09/06 23:25:29 by siuol            ###   ########.fr       */
+/*   Updated: 2025/09/07 10:45:00 by siuol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -113,13 +113,14 @@ void    Server::handlerPart(Client* client, std::string& channel, std::string& n
         code = this->_channelList[channelName]->removeUser(client);
         if (code == -1)
         {
-            std::string msg = ":<SYSTEM> PRIVMSG " + channel + " :" + CYAN + client->getNickName() + " has left the channel" + RESET;
+            std::string msg = ":<SYSTEM> PRIVMSG " + channel + " :" + CYAN + client->getNickName() + " has left the channel";
             std::string privmsg = "[CHANNEL " + channelName + "] : You have left the channel";
             if (!noti.empty())
             {
-                msg = msg + " because " + noti + RESET + "\r\n";
+                msg = msg + " because " + noti;
                 privmsg  = privmsg + " because " + noti;
             }
+            msg = msg + RESET + "\r\n";
             Notifyer::notifyBroadcast(this->_channelList[channelName], nickname, msg);
             privmsg  = RED + privmsg + RESET + "\r\n";
             Notifyer::sendMsg(this->_clientList[client->getNickName()], privmsg);
